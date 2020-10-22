2c9367d075a7994861db42a13dcffe9165f15b2a
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-22 20:36:12 +0530

    User Case7 is done

--- src/programs/empWage.java
+++ src/programs/empWage.java
@@ -1,77 +1,22 @@
 package programs;
-
+import utilities.empWageRefactorObject;
 import utilities.utility;
-
-/* Author: Tharun Srikanth Reddy
- * Date: 16-10-2020
- * Functionality: Caluculating employee daily wage
+/*
+ * Author: Tharun Srikanth Reddy
+ * Created date: 19th oct 2020
+ * Functionality: Refactor the Code to write a Class Method to
+                  Compute Employee Wage
  */
-public class empWage {
-    public static void main(String[] args) {
+public class empWage{
+    public static void main(String[] argv){
+        utility employeeWage=new utility();
+        empWageRefactorObject employeeObject=new empWageRefactorObject();
+        employeeObject.setCompany("Reliance");
+        employeeObject.setEmpRatePerHr(20);
+        employeeObject.setNumWorkingDays(20);
+        employeeObject.setMaxWorkingHrsInMonth(100);
+        int totalWages = employeeWage.WageHrs(employeeObject.getCompany(),employeeObject.getEmpRatePerHr(), employeeObject.getNumWorkingDays(), employeeObject.getMaxWorkingHrsInMonth());
+        System.out.println("Employee total Wage is :" + totalWages);
 
-        utility imp=new utility();
-        int attendance = imp.Attendance();
-        if (attendance == 1)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
-
-        /* UserCase2
-         * Functionality: Calculating Daily wage */
-        int Wage = imp.DailyWage();
-        if (Wage != 0)
-            System.out.println("Total Wage of employee for 8hrs is: " + Wage);
-        else
-            System.out.println("Employee is Absent, so Total Wage is: " +Wage);
-
-
-        /* UserCase3
-         * Functionality: Adding Part Time employee Wage
-         */
-        int TotalWage = imp.PartTimeEmp();
-        if (TotalWage == 80)
-            System.out.println("Part Time employee Wage is: " + TotalWage);
-        else if (TotalWage > 80)
-            System.out.println("Full Time employee wage is: " +TotalWage);
-        else
-            System.out.println("Employee is absent");
-
-
-        /* UserCase4
-         * Functionality: Solving by using Switch Case statement
-         */
-
-        int wages = imp.SwitchCase();
-        if (wages == 80)
-            System.out.println("Part Time employee Wage using switch case is: " + wages);
-        else if (wages > 80)
-            System.out.println("Full Time employee wage using switch case is: " +wages);
-        else
-            System.out.println("Employee is absent");
-
-
-        /* UserCase5
-         *  Functionality: Calculating employee wage for month */
-
-        int totalwages = imp.WagesforMonth();
-        if (totalwages == 1600 )
-            System.out.println("Part time Employee wage for a month : "+totalwages);
-        else if(totalwages>1600)
-            System.out.println("Full time Employee wage for a month : "+totalwages);
-        else
-            System.out.println("Employee is absent");
-
-/* UserCase6
- * Functionality: Logic to calculate employee wage
-                    for a month till the given condition
-                    reached */
-        int totalWages = imp.empWageMaxDaysHours();
-        if (totalWages == 1600 )
-            System.out.println("Part time Employee wage for a month : "+totalWages);
-        else if(totalWages>1600)
-            System.out.println("Full time Employee wage for a month : "+totalWages);
-        else
-            System.out.println("Employee is absent");
     }
-
 }
--- src/utilities/utility.java
+++ src/utilities/utility.java
@@ -1,101 +1,19 @@
 package utilities;
-/* Author: Tharun Srikanth Reddy
- * Date: 16-10-2020
- * Functionality: This is the utilities file which
-                  contains logic for the employee
-                  wage program */
+
+/* Functionality: This file contains the refactor code for
+                  calculating employee wage */
+
 
 public class utility {
+    int empHrs;
+    public int WageHrs(String company, int empRatePerHr, int NumWorkingDays, int MaxWorkingHrsInMonth) {
+        int totalWages=0,totalEmpHrs=0,totalWorkingDays=0;
 
-    /* Functionality: Getting Random number */
-    public double RandomCheck(){
-        double Check = Math.floor(Math.random() * 10) % 2;
-        return Check;
-    }
-    /* UserCase1
-     * Functionality: Logic to find whether employee
-                      is present or absent */
-    public int Attendance() {
-        int present = 0;
-        double Check = RandomCheck();
-
-        if (Check == present)
-            return 1;
-        else
-            return 0;
-    }
-
-    /* UserCase2
-     * Functionality: Logic to Calculate the
-                      Daily Wage*/
-    public int DailyWage() {
-        int empRatePerHr=20;
-        int empHrs=8;
-        int Wage;
-        int present=1;
-        double Check = RandomCheck();
-
-        if (Check == present)
-            return Wage = empRatePerHr*empHrs;
-        else
-            return 0;
-    }
-
-    /* UserCase3
-     * Functionality: Logic to Add the part time
-                      Employee wage */
-    public int PartTimeEmp() {
-        int PartTime = 1;
-        int FullTime = 2;
-        int empRatePerHr = 20;
-        int TotalWage;
-        double Check = Math.floor(Math.random() * 10) % 3;
-
-        if (Check == FullTime)
-            return TotalWage = empRatePerHr * 8;
-        else if (Check == PartTime)
-            return TotalWage = empRatePerHr * 4;
-        else
-            return 0;
-    }
-
-    /* UserCase4
-     * Functionality: Logic to find Full time wage and
-                      part time wage by using switch case*/
-
-    public int SwitchCase(){
-        int PartTime;
-        int FullTime;
-        int empRatePerHr = 20;
-        int wages;
-        double Check = Math.floor(Math.random() * 10) % 3;
-        switch ( (int) Check) {
-            case 1:
-                FullTime=8;
-                wages=empRatePerHr*FullTime;
-                return wages;
-            case 2:
-                PartTime=4;
-                wages=empRatePerHr*PartTime;
-                return wages;
-            default:
-                return 0;
-        }
-    }
-
-    /* UserCase5
-     * Functionality: Logic to calculating employee wages
-                      for a month */
-    public int WagesforMonth(){
-        int NumWorkingDays=20;
-        int wageformonth=0;
-        int empHrs=0;
-        int wages;
-        int empRatePerHr=20;
-        for (int day = 1; day<=NumWorkingDays; day++)
+        while (totalEmpHrs<MaxWorkingHrsInMonth && totalWorkingDays<NumWorkingDays)
         {
-            double randomCheck=(int)Math.floor(Math.random()*10)%3;
-            switch ((int) randomCheck)
+            totalWorkingDays++;
+            double empCheck = Math.floor(Math.random()*10)%3;
+            switch ((int) empCheck)
             {
                 case 1:
                     empHrs = 8;
@@ -106,36 +24,12 @@ public class utility {
                 default:
                     empHrs = 0;
             }
-            wages=empHrs*empRatePerHr;
-            wageformonth = wageformonth+wages;
-        }
-        return wageformonth;
-    }
-  /* UserCase6
-   * Functionality: Logic to calculate employee wage
-                    for a month till the given condition
-                    reached */
-
-    public  int empWageMaxDaysHours()
-    {
-        int empHrs,WorkingDays=20,totalWage=0;
-        int empRatePerHr=20,totalEmpHrs=0,totalworkingDays=0,MaxHrsInMonth=100;
-
-        while (totalEmpHrs<=MaxHrsInMonth && totalworkingDays<WorkingDays){
-            totalworkingDays++;
-            double randomCheck=(int)Math.floor(Math.random()*10)%3;
-            switch ((int) randomCheck){
-                case 1:
-                    empHrs =8;
-                    break;
-                case 2:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs=0;
-            }
             totalEmpHrs=totalEmpHrs+empHrs;
+            totalWages=totalEmpHrs*empRatePerHr;
         }
-        return totalWage=totalWage+(totalEmpHrs*empRatePerHr);
+        System.out.println("Name of the company is :"+company);
+        System.out.println("Working hours of the Employee :"+totalEmpHrs);
+        return totalWages;
     }
+
 }
