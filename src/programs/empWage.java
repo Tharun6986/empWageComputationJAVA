ec44ca4d0b8c0238fc8c0863bcc9ab410e64d667
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-22 19:53:29 +0530

    UserCase3 is done

--- src/programs/empWage.java
+++ src/programs/empWage.java
@@ -1,10 +1,10 @@
 package programs;
 
-import utilities.utility;
+import utilities. utility;
 
 /* Author: Tharun Srikanth Reddy
  * Date: 16-10-2020
- * Functionality: To check whether employee is present or absent
+ * Functionality: Caluculating employee daily wage
  */
 public class empWage {
     public static void main(String[] args) {
@@ -16,6 +16,27 @@ public class empWage {
         else
             System.out.println("Employee is Absent");
 
+        /* UserCase2
+         * Functionality: Calculating Daily wage */
+        int Wage = imp.DailyWage();
+        if (Wage != 0)
+            System.out.println("Total Wage of employee for 8hrs is: " + Wage);
+        else
+            System.out.println("Employee is Absent, so Total Wage is: " +Wage);
+
+
+        /* UserCase3
+         * Functionality: Adding Part Time employee Wage
+         */
+        int TotalWage = imp.PartTimeEmp();
+        if (TotalWage == 80)
+            System.out.println("Part Time employee Wage is: " + TotalWage);
+        else if (TotalWage > 80)
+            System.out.println("Full Time employee wage is: " +TotalWage);
+        else
+            System.out.println("Employee is absent");
 
     }
+
+
 }
--- src/utilities/utility.java
+++ src/utilities/utility.java
@@ -7,14 +7,54 @@ package utilities;
 
 public class utility {
 
-    /* Functionality: Logic to find whether employee
+    /* Functionality: Getting Random number */
+    public double RandomCheck(){
+        double Check = Math.floor(Math.random() * 10) % 2;
+        return Check;
+    }
+    /* UserCase1
+     * Functionality: Logic to find whether employee
                       is present or absent */
     public int Attendance() {
         int present = 0;
-        double Check = Math.floor(Math.random() * 10) % 2;
+        double Check = RandomCheck();
 
         if (Check == present)
             return 1;
+        else
+            return 0;
+    }
+
+    /* UserCase2
+     * Functionality: Logic to Calculate the
+                      Daily Wage*/
+    public int DailyWage() {
+        int empRatePerHr=20;
+        int empHrs=8;
+        int Wage;
+        int present=1;
+        double Check = RandomCheck();
+
+        if (Check == present)
+            return Wage = empRatePerHr*empHrs;
+        else
+            return 0;
+    }
+
+    /* UserCase3
+     * Functionality: Logic to Add the part time
+                      Employee wage */
+    public int PartTimeEmp() {
+        int PartTime = 1;
+        int FullTime = 2;
+        int empRatePerHr = 20;
+        int TotalWage;
+        double Check = Math.floor(Math.random() * 10) % 3;
+
+        if (Check == FullTime)
+            return TotalWage = empRatePerHr * 8;
+        else if (Check == PartTime)
+            return TotalWage = empRatePerHr * 4;
         else
             return 0;
     }
