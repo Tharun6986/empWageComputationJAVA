09891bcb417741f5a7e0a7a8fb4944e47700f7b5
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-22 20:39:29 +0530

    User Case8 is done

--- src/programs/empWage.java
+++ src/programs/empWage.java
@@ -5,18 +5,25 @@ import utilities.utility;
  * Author: Tharun Srikanth Reddy
  * Created date: 19th oct 2020
  * Functionality: Refactor the Code to write a Class Method to
-                  Compute Employee Wage
+                  Compute Employee Wage for multiple companies.
  */
 public class empWage{
     public static void main(String[] argv){
         utility employeeWage=new utility();
-        empWageRefactorObject employeeObject=new empWageRefactorObject();
-        employeeObject.setCompany("Reliance");
-        employeeObject.setEmpRatePerHr(20);
-        employeeObject.setNumWorkingDays(20);
-        employeeObject.setMaxWorkingHrsInMonth(100);
-        int totalWages = employeeWage.WageHrs(employeeObject.getCompany(),employeeObject.getEmpRatePerHr(), employeeObject.getNumWorkingDays(), employeeObject.getMaxWorkingHrsInMonth());
-        System.out.println("Employee total Wage is :" + totalWages);
+        empWageRefactorObject ReliancEemployeeWage=new empWageRefactorObject();
+        ReliancEemployeeWage.setCompany("Reliance");
+        ReliancEemployeeWage.setEmpRatePerHr(20);
+        ReliancEemployeeWage.setNumWorkingDays(20);
+        ReliancEemployeeWage.setMaxWorkingHrsInMonth(100);
+        int ReliancetotalWages = employeeWage.WageHrs(ReliancEemployeeWage.getCompany(),ReliancEemployeeWage.getEmpRatePerHr(), ReliancEemployeeWage.getNumWorkingDays(), ReliancEemployeeWage.getMaxWorkingHrsInMonth());
+        System.out.println("Reliance Employee total Wage is :" + ReliancetotalWages);
 
+        empWageRefactorObject dmartemployeewage=new empWageRefactorObject();
+        dmartemployeewage.setCompany("DMart");
+        dmartemployeewage.setEmpRatePerHr(17);
+        dmartemployeewage.setNumWorkingDays(25);
+        dmartemployeewage.setMaxWorkingHrsInMonth(90);
+        int dmartTotalWage = employeeWage.WageHrs(dmartemployeewage.getCompany(),dmartemployeewage.getEmpRatePerHr(), dmartemployeewage.getNumWorkingDays(), dmartemployeewage.getMaxWorkingHrsInMonth());
+        System.out.println("DMart Employee total Wage is :" + dmartTotalWage);
     }
 }
